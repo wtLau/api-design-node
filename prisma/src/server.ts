e5011f9273f06a710632ce7bf71cb0887853f8f5
wtlau <wtlau.dev@gmail.com>
2022-11-20 19:27:59 -0800

    creating a custom middleware

--- prisma/src/server.ts
+++ prisma/src/server.ts
@@ -1,7 +1,19 @@
 import express from 'express'
 import router from './router'
-
+import cors from 'cors'
+import morgan from 'morgan'
 const app = express()
+
+app.use(cors())
+// Logging
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true})) // use to intercep query parameter
+
+app.use((req, res) => {
+	req.shhhhh_secret = 'doggy'
+	next()
+})
 
 app.get('/', (req, res) => {
 	console.log('hello from express')
