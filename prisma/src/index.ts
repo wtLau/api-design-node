6841b74792d866e5c9ba43c4ca08a90f18816ed2
Brian Lau <blau@appannie.com>
2022-11-22 08:20:59 -0800

    auth: creating a jwt

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -1,5 +1,8 @@
-import app from "./server";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import app from './server'
 
 app.listen(3001, () => {
-  console.log("hello on http://localhost:3001");
-});
+	console.log('hello on http://localhost:3001')
+})
