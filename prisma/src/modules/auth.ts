5bbdb7df0281aad35c0a0fe57adf6af2aa0c5af2
Brian Lau <blau@appannie.com>
2022-12-02 09:16:07 -0800

    authorization headers

--- prisma/src/modules/auth.ts
+++ prisma/src/modules/auth.ts
@@ -14,4 +14,23 @@ export const protect = (req, res, next) => {
 		res.json({message: 'not authorized'})
 		return
 	}
+
+	const [, token] = bearer.split(' ')
+
+	if (!token) {
+		res.status(401)
+		res.json({message: 'not valid token'})
+		return
+	}
+
+	try {
+		const user = jwt.verify(token, process.env.JWT_SECRET)
+		req.user = user
+		next()
+	} catch (e) {
+		console.log(e)
+		res.status(401)
+		res.json({message: 'not valid token'})
+		return
+	}
 }
